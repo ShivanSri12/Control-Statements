468cc0340030fac3b2583bedda408daaa9bfce54
ShivanSri12 <Shivansri10@gmail.com>
2023-09-19 09:04:54 +0530

    Initial Commit

---
+++ Control Statements/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//If STATEMENT
+
+//if (20 > 18)
+//{
+//    Console.WriteLine("20 is greater than 18");
+//}
+
+//int x = 20;
+//int y = 18;
+//if (x > y)
+//{
+//    Console.WriteLine("x is greater than y");
+//}
+
+//ELSE STATEMENT
+//int time = 20;
+//if (time < 18)
+//{
+//    Console.WriteLine("Good day.");
+//}
+//else
+//{
+//    Console.WriteLine("Good evening.");
+//}
+
+//ELSE IF STATEMENT
+//int time1 = 22;
+//if (time1 < 10)
+//{
+//    Console.WriteLine("Good morning.");
+//}
+//else if (time1 > 20)
+//{
+//    Console.WriteLine("Good day.");
+//}
+//else
+//{
+//    Console.WriteLine("Good evening.");
+//}
+
+// SWITCH STATEMENT
+
+//int day = 4;
+//switch (day)
+//{
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+//    case 3:
+//        Console.WriteLine("Wednesday");
+//        break;
+//    case 4:
+//        Console.WriteLine("Thursday");
+//        break;
+//    case 5:
+//        Console.WriteLine("Friday");
+//        break;
+//    case 6:
+//        Console.WriteLine("Saturday");
+//        break;
+//    case 7:
+//        Console.WriteLine("Sunday");
+//        break;
+//}
+
+// WHILE LOOP
+
+//int i = 0;
+//while (i < 5)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+// FOR LOOP
+
+//for (int k = 0; k < 5; k++)
+//{
+//    Console.WriteLine(k);
+//}
+
+//Arrays
+
+//string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+//Console.WriteLine(cars[2]);
+
+//change an array element
+
+//string[] test = { "Volvo", "BMW", "Ford", "Mazda" };
+//test[0] = "Opel";
+//Console.WriteLine(test[0]);
+
+//Array Length
+
+string[] carsdata = { "Audi", "BMW", "Benz" };
+int arrayLength = carsdata.Length;
+Console.WriteLine(arrayLength);
